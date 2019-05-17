6eeca0b99926a008e71087d2a9b3f0fa904ad172
Eduardo Sirangelo <eduardo@bookinghair.com>
2019-05-16 22:45:59 -0300

    Criando componentes de botão e display

--- App.js
+++ App.js
@@ -8,42 +8,62 @@
 
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import Button from './src/components/Button';
+import Display from './src/components/Display';
 
 type Props = {};
 export default class App extends Component<Props> {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
-    );
-  }
+
+    state = {
+        displayValue: '0'
+    };
+
+    addDigit = n => {
+        this.setState({ displayValue: n })
+    };
+
+    clearMemory = () => {
+        this.setState({ displayValue: '0' })
+    };
+
+    setOperation = operation => {
+
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Display value={this.state.displayValue} />
+                <View style={styles.buttons}>
+                    <Button label='AC' triple onClick={this.clearMemory}/>
+                    <Button label='/' operation onClick={this.setOperation} />
+                    <Button label='7' onClick={this.addDigit} />
+                    <Button label='8' onClick={this.addDigit} />
+                    <Button label='9' onClick={this.addDigit} />
+                    <Button label='*' operation onClick={this.setOperation} />
+                    <Button label='4' onClick={this.addDigit} />
+                    <Button label='5' onClick={this.addDigit} />
+                    <Button label='6' onClick={this.addDigit} />
+                    <Button label='-' operation onClick={this.setOperation} />
+                    <Button label='1' onClick={this.addDigit} />
+                    <Button label='2' onClick={this.addDigit} />
+                    <Button label='3' onClick={this.addDigit} />
+                    <Button label='+' operation onClick={this.setOperation} />
+                    <Button label='0' double onClick={this.addDigit} />
+                    <Button label='.' onClick={this.addDigit} />
+                    <Button label='=' operation onClick={this.setOperation} />
+                </View>
+            </View>
+        );
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1,
+    },
+    buttons: {
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    }
 });
---
+++ README.md
@@ -0,0 +1,19 @@
+# Calculadora
+
+*A sample app using react native for iOS & Android*
+
+## Instalação
+
+**Passo 1:** clonar aplicação
+
+```
+git clone git@github.com:esirangelo-react-native/calculadora.git
+```
+
+**Passo 2:** install node modules
+
+```
+npm install
+```
+
+## Executando aplicação
